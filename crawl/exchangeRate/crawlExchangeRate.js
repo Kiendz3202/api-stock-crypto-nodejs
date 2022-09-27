808a4508c216a0ecea4edb6e008eb68a9c09987e
Manh Kien <manhkien3202@gmail.com>
2022-09-27 16:27:09 +0700

    abbank

--- crawl/exchangeRate/crawlExchangeRate.js
+++ crawl/exchangeRate/crawlExchangeRate.js
@@ -29,9 +29,8 @@ const collectDataAbBank = async (url) => {
 		await page.setUserAgent(
 			'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36'
 		);
-		await page.goto(url, { waitUntil: 'load' });
-
-		await page.waitForTimeout(2000);
+		await page.setDefaultNavigationTimeout(0);
+		await page.goto(url);
 
 		return page.evaluate(async () => {
 			const $ = document.querySelector.bind(document);
