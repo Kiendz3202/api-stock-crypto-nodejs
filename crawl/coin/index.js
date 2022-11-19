f36bb0ae5f8b8599cb8d23e0401b131c226b7a84
Manh Kien <manhkien3202@gmail.com>
2022-11-19 15:16:43 +0700

    fix coin

--- crawl/coin/index.js
+++ crawl/coin/index.js
@@ -130,7 +130,7 @@ const coinRunAll = asyncHandler(async () => {
 								// .then((doc) => console.log(doc?.symbol))
 								.catch((err) => console.log(err));
 						});
-				}, 2000 * index);
+				}, 3000 * index);
 			} catch (error) {
 				console.log(error);
 			}
