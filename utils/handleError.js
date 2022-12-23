15e95604262f9cfd3ba2a482d5401abe897422ad
Manh Kien <manhkien3202@gmail.com>
2022-12-23 13:28:20 +0700

    fix

--- utils/handleError.js
+++ utils/handleError.js
@@ -3,7 +3,7 @@ const Error = require('../model/error/errorModel');
 const uploadErrorToDb = async (messageError) => {
 	const timeUpdate = Math.floor(Date.now() / 1000);
 	Error.findOneAndUpdate(
-		{ message: messageError },
+		{ timeUpdate: timeUpdate },
 		{
 			message: messageError,
 			timeUpdate: timeUpdate,
