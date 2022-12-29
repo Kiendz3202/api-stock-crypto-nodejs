a96f0812448038f212cceeb4ae9629c46140ef18
Manh Kien <manhkien3202@gmail.com>
2022-12-29 11:32:25 +0700

    fix

--- crawl/finalCombine.js
+++ crawl/finalCombine.js
@@ -30,8 +30,8 @@ const runCrawlCoin = async () => {
 };
 
 const runCrawlAllListStocks = async () => {
-	// cron.schedule('*/10 9-14 * * 1-5', async () => {
-	cron.schedule('*/10 * * * *', async () => {
+	cron.schedule('*/10 9-14 * * 1-5', async () => {
+		// cron.schedule('*/10 * * * *', async () => {
 		updateAllListStocks();
 	});
 };
